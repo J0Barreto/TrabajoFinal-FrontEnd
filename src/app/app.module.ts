11f2565a25035cf2adcf2c853445244502c8462c
JoseBarreto <barretojantonio@gmail.com>
2022-08-21 19:59:01 -0300

    Creacion del acerca de y mod de stilos de perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PerfilComponent
+    PerfilComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
